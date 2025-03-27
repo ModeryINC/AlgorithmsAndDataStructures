353c052247ab3c0cd334f9ce70c9bcbf13781426
ModeryINC <kamykcygan@gmail.com>
2025-03-27 23:56:00 +0100

    [#4] Zadanie B2

--- B2/Destination.cs
+++ B2/Destination.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace B2 {
     class Destination(int distance, string portName)
     {
-        private readonly int distance = distance;
+        private readonly int distance = (distance > 0) ? distance : 0;
         private readonly string portName = portName;
 
         public int GetDistance() {
