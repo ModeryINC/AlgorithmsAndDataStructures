2cf695163eb17b317c739aacb8b39d76a66af260
ModeryINC <kamykcygan@gmail.com>
2025-03-27 23:56:32 +0100

    [#5] Zadanie B3

---
+++ B3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Collections.Generic;
+using System;
+
+namespace B3 {
+    class Program {
+        internal static readonly string[] sourceArray = ["Lithuania", "Latvia", "Estonia"];
+
+        static void Main(string[] args) {
+            HighestPeak austriaHighestPeak = new("", "", 0);
+            int peakHeigthSum = 0;
+            int peakCount = 0;
+            List<HighestPeak> peaksList = Homework.CreateList();
+            List<HighestPeak> peaksListSecond = [];
+            Dictionary<string, double> countryPopulations = Homework.CreateDictionary();
+            foreach(HighestPeak peak in peaksList) {
+                if (peak.Country == "Austria" && peak.Elevation > austriaHighestPeak.Elevation) {
+                    austriaHighestPeak = peak;
+                }
+                if (4000 < peak.Elevation && peak.Elevation < 5000 ) {
+                    peaksListSecond.Add(peak);
+                }
+                peakHeigthSum += peak.Elevation;
+                peakCount++;
+            }
+            Console.Write("\n\n#01\n");
+            austriaHighestPeak.ShowInfo();
+            
+            Console.Write("\n\n#02\n");
+            peaksList[^2].ShowInfo();
+            
+            Console.Write("\n\n#03\n > ");
+            Console.Write("Średnia wysokość: " + peakHeigthSum / peakCount);
+            
+            Console.Write("\n\n#04\n");
+            peaksList.ForEach(peak => peak.ShowInfo());
+            
+            Console.Write("\n\n#05\n");
+            peaksList = [.. peaksList.OrderBy(peak => peak.Elevation)];
+            for (int i = 0; i < 10; i++) peaksList[i].ShowInfo();
+            
+            Console.Write("\n\n#06\n > ");
+            Console.Write(countryPopulations.FirstOrDefault(country => country.Value == 5.5).Key);
+            
+            Console.Write("\n\n#07\n > ");
+            countryPopulations.Add("newCountry", 50.7);
+            Console.Write(countryPopulations.Count);
+            
+            Console.Write("\n\n#08\n > ");
+            Console.Write(countryPopulations.Average(country => country.Value));
+            
+            Console.Write("\n\n#09\n > ");
+            Console.Write(string.Join(" | ", countryPopulations.Where(country => 10 < country.Value && country.Value < 20).ToDictionary().Keys));
+            
+            Console.Write("\n\n#10\n > ");
+            Console.Write(countryPopulations.Where(country => sourceArray.Contains(country.Key)).ToDictionary().Values.Sum());
+        }
+    }
+}
---
+++ newProject.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+
+set /p project=Podaj nazwe projektu: 
+
+dotnet new console -o %project%
+dotnet sln AlgorithmsAndDataStructures.sln add ./%project%/%project%.csproj
