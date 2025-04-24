573c70872d36ce14f598530d0660e1299b0ea42d
ModeryINC <kamykcygan@gmail.com>
2025-04-24 11:59:18 +0200

    [#13] Zadanie B5

--- B5/Program.cs
+++ B5/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Threading;
 using System.Linq;
 using System;
-using System.Diagnostics;
 
 namespace B5 {
     class Program {
@@ -12,13 +12,13 @@ namespace B5 {
             Stopwatch stopwatch = Stopwatch.StartNew();
             Task<List<int>> taskFirst = PrimaryNumbers.Find();
             Task<List<int>> taskSecond = PrimaryNumbers.Find(833334);
-            List<int> PrimaryNumbersList = [2, 3];
+            List<int> PrimaryNumbersList = [2, 3, 5];
             PrimaryNumbersList.AddRange(await taskFirst);
             PrimaryNumbersList.AddRange(await taskSecond);
             stopwatch.Stop();
             Console.Write(
-                string.Join(" | ", PrimaryNumbersList.Order().Take(20))
-                + "\nCzas wykonywania: " + Math.Round(stopwatch.ElapsedMilliseconds / 1000.0)
+                "| " + string.Join(" | ", PrimaryNumbersList.Order().Take(20)) + " |\n"
+                + "Czas wykonywania: " + Math.Round(stopwatch.ElapsedMilliseconds / 1000.0)
                 + "s | " + stopwatch.ElapsedMilliseconds + "ms\nIlość liczb: " + PrimaryNumbersList.Count + "\n");
         }
     }
