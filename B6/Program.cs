a5aa4c94f6c4eda2169f18cff7ad95e007af9c0f
ModeryINC <kamykcygan@gmail.com>
2025-05-05 18:06:42 +0200

    [#16] Zadanie B6

---
+++ B6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System;
+/* uwaga: jesli zdarzy sie, ze system nie bedzie chcial Panstwu wykrywac biblioteki System.Diagnostics, prosze sprobowac nastepujacego rozwiazania:
+ * w solution explorerze (menu po prawej stronie) kliknąć prawym przyciskiem myszy na References -> Add Reference -> wyszukać "System" 
+ * -> zaznaczyć (ważne!) ptaszkiem z lewej strony i kliknąć ok
+ * */
+
+namespace B6 {
+    class Program {
+        static void Main(string[] args) {
+            SearchSortEngine testList = new(1000);
+            testList.SaveList("unsorted");
+            // selection sort
+            testList.SelectionSort();
+            testList.SaveList("selection");
+            // insertion sort
+            testList.Reshuffle();
+            testList.InsertionSort();
+            testList.SaveList("insertion");
+            // bubble sort
+            testList.Reshuffle();
+            testList.BubbleSort();
+            testList.SaveList("bubble");
+            // linear search
+            testList.Reshuffle();
+            Console.WriteLine("Searching for numbers... [linear search]");
+            for (int i = 10; i < 20; i++) {
+                Console.WriteLine(i + " - " + testList.LinearSearch(i));
+            }
+            // jump search
+            testList.SelectionSort(); // we need to sort the list first
+            Console.WriteLine("Searching for numbers... [jump search]");
+            for (int i = 10; i < 20; i++) {
+                Console.WriteLine(i + " - " + testList.JumpSearch(i));
+            }
+
+            // ************************* performance **************************
+            Console.WriteLine("\nPerformance tests");
+            SearchSortEngine timeTest = new(10000); // we want slightly longer list to get more reliable results 
+            Stopwatch stopwatch; // this is the time measurement device we will use
+            double measurement; // here results will be saved
+            // selection sort
+            stopwatch = Stopwatch.StartNew(); // start the measurement
+            timeTest.SelectionSort(); // do your job
+            stopwatch.Stop(); // stop the measurement
+            measurement = Convert.ToDouble(stopwatch.ElapsedTicks) / Stopwatch.Frequency; // read the measurements value (in seconds)
+            Console.WriteLine("Execution time for selection sort: {0:0.00} miliseconds", 1000 * measurement); // for convenience, we scale by 1000 and give value in ms
+            // insertion sort
+            timeTest.Reshuffle(); // shuffling is not included in the measurement, since we want to compare sorting algorithms only
+            stopwatch = Stopwatch.StartNew();
+            timeTest.InsertionSort();
+            stopwatch.Stop();
+            measurement = Convert.ToDouble(stopwatch.ElapsedTicks) / Stopwatch.Frequency;
+            Console.WriteLine("Execution time for insertion sort: {0:0.00} miliseconds", 1000 * measurement);
+            // bubble sort
+            timeTest.Reshuffle();
+            stopwatch = Stopwatch.StartNew();
+            timeTest.BubbleSort();
+            stopwatch.Stop();
+            measurement = Convert.ToDouble(stopwatch.ElapsedTicks) / Stopwatch.Frequency;
+            Console.WriteLine("Execution time for bubble sort: {0:0.00} miliseconds", 1000 * measurement);
+        }
+    }
+}
---
+++ B6/SearchSortEngine.cs
@@ -0,0 +1,106 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.ComponentModel;
+
+namespace B6 {
+    class SearchSortEngine {
+        private List<int> numbers;
+        private readonly int size;
+        public SearchSortEngine(int n){
+            //constructor
+            if (n > 0) size = n;
+            else {
+                Console.WriteLine("List size cannot be negative - defaulting to 10 instead");
+                size = 10;
+            }
+            numbers = new List<int>(size);
+            Random rnd = new();
+            for (int i = 0; i < size; i++) {
+                numbers.Add(rnd.Next(10000));
+            }
+        }
+        // utility methods
+        public void Reshuffle() {
+            // shuffle list contents 
+            // Fisher-Yates shuffle algorithm
+            Random rnd = new();
+            int j = size;
+            while (j > 1) {
+                int i = rnd.Next(j);
+                j--;
+                (numbers[j], numbers[i]) = (numbers[i], numbers[j]);
+            }
+        }
+        public void SaveList(string filename) {
+            // save list to "filename.txt"
+            // uwaga dla Panstwa - pliki domyslnie zapisuja sie w folderze aplikacji
+            // typowo jest to katalog roboczy -> bin -> Debug (-> netcoreappX.Y jesli taki katalog wystepuje)
+            try {
+                List<String> listStr = numbers.ConvertAll<string>(i => i.ToString()); // convert list<int> to list<string>
+                File.WriteAllLines("./B6/" + filename + ".txt", [.. listStr]); // write the list 
+            }
+            catch (IOException e) {
+                Console.WriteLine("The file could not be opened or a writing error has occurred:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void SelectionSort() {
+            for (int i = 0; i < size; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < size; j++) {
+                    if (numbers[minIndex] > numbers[j]) minIndex = j;
+                }
+                if (minIndex != i) (numbers[i], numbers[minIndex]) = (numbers[minIndex], numbers[i]);
+            }
+        }
+        public void InsertionSort() {
+            for (int i = 1; i < size; i++) {
+                int keyValue = numbers[i];
+                int insertIndex = i;
+                for (int j = i - 1; j >= 0; j--) {
+                    if (numbers[j] > keyValue) {
+                        insertIndex = j;
+                    } else break;
+                }
+                if (insertIndex != i) {
+                    numbers.RemoveAt(i);
+                    numbers.Insert(insertIndex, keyValue);
+                }
+            }
+        }
+        public void BubbleSort() {
+            for (int i = size; i > 0; i--) {
+                for (int j = 1; j < i; j++) {
+                    if (numbers[j] < numbers[j-1]) (numbers[j], numbers[j-1]) = (numbers[j-1], numbers[j]);
+                }
+            }
+        }
+        public bool LinearSearch(int number) {
+            for(int i = 0; i < size; i++) {
+                if (numbers[i] == number) return true;
+            }
+            return false;
+        }
+        public bool JumpSearch(int number) {
+            int jumpLength = (int)Math.Sqrt(size),
+                prev = jumpLength;
+            for (int i = jumpLength; i < size; i += jumpLength) {
+                if (numbers[i - jumpLength] < number && number < numbers[i]) {
+                    for (int j = i - jumpLength; j < i; j++) {
+                        if (numbers[j] == number) return true;
+                    }
+                    return false;
+                }
+            }
+            // while (prev < size - jumpLength && numbers[prev] < number && number < numbers[prev + jumpLength]) {
+            //     prev += jumpLength;
+            // }
+            // for (int i = prev; i <= prev + jumpLength && i < size; i++) {
+            //     if (numbers[i] == number) return true;
+            // }
+            return false;
+        }
+    }
+}
