a1b9e2cf6afdcb36c95cc82dcf95541c4e89aca5
ModeryINC <kamykcygan@gmail.com>
2025-06-14 01:12:09 +0200

    [#25 Zadanie BP

--- BP/graph.cpp
+++ BP/graph.cpp
@@ -15,6 +15,22 @@ class Graph {
     protected:
         vector<pair<int, string>> vertices;
         vector<connection> connections;
+        void addConnection(int start, int end, int weight) {
+            int start_id = this->find(start),
+                end_id = this->find(end);
+            if(start_id != -1 || end_id != -1)
+                throw invalid_argument("Start or end isnt in ids");
+            connections.emplace_back(vertices[start_id], vertices[end_id], weight);
+        }
+        void addVertex(string name) {
+            int id = this->find("max") + 1;
+            vertices.emplace_back(id, name);
+        }
+        void addVertex(int id, string name) {
+            if(this->find(id) != -1)
+                throw invalid_argument("This id already exist!");
+            vertices.emplace_back(id, name);
+        }
     private:
         int find(int id) {
             try {
@@ -25,6 +41,21 @@ class Graph {
                     if(vertices[i].first = id)
                         return i;
                 return -1;
+            }
+        }
+        int find(string text) {
+            if((text) == "max") {
+                int max = 0;
+                for(int i = 0; i < vertices.size(); i++)
+                    if(vertices[i].first > max)
+                        max = vertices[i].first;
+                return max;
+            } else if((text) == "min") {
+                int min = 0;
+                for(int i = 0; i < vertices.size(); i++)
+                    if(vertices[i].first < min)
+                        min = vertices[i].first;
+                return min;
             }
         }
     public:
@@ -46,12 +77,5 @@ class Graph {
             for(int i = 0; i < names.size(); i++)
                 vertices.emplace_back(ids[i], names[i]);
             this->connections = connections;
-        }
-        void addConnection(int start, int end, int weight) {
-            int start_id = this->find(start),
-                end_id = this->find(end);
-            if(start_id != -1 || end_id != -1)
-                throw invalid_argument("Start or end isnt in ids");
-            connections.emplace_back(vertices[start_id], vertices[end_id], weight);
         }
 };
--- BP/map.cpp
+++ BP/map.cpp
@@ -1,5 +1,15 @@
 #include<vector>
+#include<set>
 
+#include"struct.h"
 #include"graph.h"
 
-class Map {};
+class Map : Graph {
+    private:
+        int level = 1;
+        set<int> visitedVertex;
+    public:
+        Map generateMap() {
+            
+        }
+};
